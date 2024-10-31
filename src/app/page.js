89d6187cdf8b955308489453c5ccbe1dc7a311db
jarray-kais <jarraykais1@gmail.com>
2024-10-31 21:53:56 +0100

    styling / import images
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
-export default function Home() {
+import Mainfeature from "@/components/Mainfeature";
+
+
+export default function Home(){
   return (
     <>
-   
+    <div className='dots'>
+   <Mainfeature />
+
+   </div>
     </>
   );
 }
